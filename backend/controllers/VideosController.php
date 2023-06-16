783a04acb0822fa56dbda8a15f99d00ca939a1ca
Piotr Próchnicki <piotr.prochnicki@gmail.com>
2023-06-16 11:52:26 +0200

    Feature-fix/added some generated code snippets for videos

---
+++ backend/controllers/VideosController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace backend\controllers;
+
+use common\models\Videos;
+use yii\data\ActiveDataProvider;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * VideosController implements the CRUD actions for Videos model.
+ */
+class VideosController extends Controller
+{
+    /**
+     * @inheritDoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Lists all Videos models.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Videos::find(),
+            /*
+            'pagination' => [
+                'pageSize' => 50
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    'video_id' => SORT_DESC,
+                ]
+            ],
+            */
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Videos model.
+     * @param string $video_id Video ID
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($video_id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($video_id),
+        ]);
+    }
+
+    /**
+     * Creates a new Videos model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return string|\yii\web\Response
+     */
+    public function actionCreate()
+    {
+        $model = new Videos();
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'video_id' => $model->video_id]);
+            }
+        } else {
+            $model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing Videos model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $video_id Video ID
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($video_id)
+    {
+        $model = $this->findModel($video_id);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'video_id' => $model->video_id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Videos model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $video_id Video ID
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($video_id)
+    {
+        $this->findModel($video_id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Videos model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $video_id Video ID
+     * @return Videos the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($video_id)
+    {
+        if (($model = Videos::findOne(['video_id' => $video_id])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
