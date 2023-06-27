4628fd4bcababf1d18f96486f705644a4d4fa8bc
Piotr Próchnicki <piotr.prochnicki@gmail.com>
2023-06-27 10:56:07 +0200

    Feature-fix/modified controller

--- backend/controllers/VideosController.php
+++ backend/controllers/VideosController.php
@@ -108,7 +108,7 @@ class VideosController extends Controller
         $model = $this->findModel($id);
 
         if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'video_id' => $model->video_id]);
+            return $this->redirect(['update', 'video_id' => $model->video_id]);
         }
 
         return $this->render('update', [
