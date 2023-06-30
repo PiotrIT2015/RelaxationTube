b908147b755dbc42f85a5997b566d2f5f53cb11c
Piotr Próchnicki <piotr.prochnicki@gmail.com>
2023-06-30 11:26:36 +0200

    Feature-fix/add clickable functionality to 'video id'

--- backend/views/videos/_video_item.php
+++ backend/views/videos/_video_item.php
@@ -2,14 +2,17 @@
 /** @var $model \common\models\Videos */
 
 use yii\helpers\StringHelper;
+use yii\helpers\Url;
 ?>
 
 <div class="media">
-    <div class="embed-responsive embed-responsive-16by9 mr-2" style="width: 120px">
-        <video class="embeded-responsive-item" 
-        poster="<?php echo $model->getThumbnailLink() ?>"
-        src="<?php echo $model->getVideoLink() ?>" ></video>
-    </div>
+    <a href="<?php echo Url::to(['/videos/update','id'=>$model->video_id])?>">
+        <div class="embed-responsive embed-responsive-16by9 mr-2" style="width: 120px">
+            <video class="embeded-responsive-item" 
+            poster="<?php echo $model->getThumbnailLink() ?>"
+            src="<?php echo $model->getVideoLink() ?>" ></video>
+        </div>
+    </a>
     <div class="media-body">
         <h6 class="mt-0"><?php echo $model->title ?></h6>
         <?php echo StringHelper::truncateWords($model->description, 10)?>
