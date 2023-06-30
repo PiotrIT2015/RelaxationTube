ceabc185df2ca092fd8ad3ef96da2ee16dc4b95c
Piotr Próchnicki <piotr.prochnicki@gmail.com>
2023-06-30 11:05:14 +0200

    Feature-fix/add view to video-item

--- backend/views/videos/_video_item.php
+++ backend/views/videos/_video_item.php
@@ -1,3 +1,17 @@
 <?php
+/** @var $model \common\models\Videos */
+
+use yii\helpers\StringHelper;
 ?>
-Video Item
+
+<div class="media">
+    <div class="embed-responsive embed-responsive-16by9 mr-3" style="width: 140px">
+        <video class="embeded-responsive-item" 
+        poster="<?php echo $model->getThumbnailLink() ?>"
+        src="<?php echo $model->getVideoLink() ?>" ></video>
+    </div>
+    <div class="media-body">
+        <h6 class="mt-0"><?php echo $model->title ?></h6>
+        <?php echo StringHelper::truncateWords($model->description, 10)?>
+    </div>
+</div>
