c303e116c4c88c53b6629588da5e6ab12de158b3
Piotr Próchnicki <piotr.prochnicki@gmail.com>
2023-07-04 10:45:49 +0200

    Feature-fix/modyfied frontend

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -11,6 +11,7 @@ return [
     //'name' => 'Relaxation Space',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => '/video/index',
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'request' => [
